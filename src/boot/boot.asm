3e91c6f468ebfa7dad4df8aed1674f74a248aada
Steven Kang <kangweibaby@163.com>
2020-08-18 15:05:30 +0800

    add read file from fab12

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -14,18 +14,41 @@ _start:
     mov si, MESSAGE_LOADING
     call bios_print
 
-    BIOS_RESET_FLOPPY
+    FAT12_READ_FILE
 
+read_failure:
+    mov si, MESSAGE_RETURN
+    call bios_print
+    mov si, MESSAGE_FAILURE
+    call bios_print
     jmp $
 
-read_sector:
-    ret
+read_success:
+    mov si, MESSAGE_RETURN
+    call bios_print
+    mov si, MESSAGE_SUCCESS
+    call bios_print
+    jmp MEMORY_SEGMENT:MEMORY_OFFSET
 
+read_not_found:
+    mov si, MESSAGE_RETURN
+    call bios_print
+    mov si, MESSAGE_NOFOUND
+    call bios_print
+    jmp $
 
 BIOS_PRINT_FUNCTION
 
-MESSAGE_LOADING:
-    db "Booting...", 0
+MEMORY_SEGMENT  equ LOADER_SEGMENT
+MEMORY_OFFSET   equ LOADER_OFFSET
+
+FILENAME db "LOADER  BIN", 0
+MESSAGE_RETURN db 13, 10, 0
+MESSAGE_LOADING db "Booting", 0
+MESSAGE_FAILURE db "Failure", 0
+MESSAGE_SUCCESS db "Success", 0
+MESSAGE_NOFOUND db "Nofound", 0
+MESSAGE_FOUND db "Found", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
