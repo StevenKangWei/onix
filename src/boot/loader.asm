656b8ca8eb49dbc2cad3229d769e48cc0419d952
Steven Kang <kangweibaby@163.com>
2020-08-18 15:09:46 +0800

    update loader

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -14,11 +14,40 @@ _start:
     mov si, MESSAGE_LOADING
     call bios_print
 
-    BIOS_RESET_FLOPPY
+    FAT12_READ_FILE
 
+    jmp $
+
+read_failure:
+    mov si, MESSAGE_RETURN
+    call bios_print
+    mov si, MESSAGE_FAILURE
+    call bios_print
+    jmp $
+
+read_success:
+    mov si, MESSAGE_RETURN
+    call bios_print
+    mov si, MESSAGE_SUCCESS
+    call bios_print
+    jmp MEMORY_SEGMENT:MEMORY_OFFSET
+
+read_not_found:
+    mov si, MESSAGE_RETURN
+    call bios_print
+    mov si, MESSAGE_NOFOUND
+    call bios_print
     jmp $
 
 BIOS_PRINT_FUNCTION
 
-MESSAGE_LOADING:
-    db "Loading...", 0
+MEMORY_SEGMENT  equ KERNEL_SEGMENT
+MEMORY_OFFSET   equ KERNEL_OFFSET
+
+FILENAME db "KERNEL  BIN", 0
+MESSAGE_RETURN db 13, 10, 0
+MESSAGE_LOADING db "Onix OS is Loading...", 0
+MESSAGE_FAILURE db "Failure to read kernel...", 0
+MESSAGE_SUCCESS db "Success to read kernel...", 0
+MESSAGE_NOFOUND db "Kernel is not found...", 0
+MESSAGE_FOUND db "Found kernel in root directory...", 0
