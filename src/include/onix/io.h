00a4b9f6cedba6828c747c6d3e26153e1d073639
Steven Kang <kangweibaby@163.com>
2020-08-19 12:40:05 +0800

    add io.c

---
+++ src/include/onix/io.h
@@ -0,0 +1,9 @@
+#ifndef ONIX_IO_H
+#define ONIX_IO_H
+
+#include <onix/type.h>
+
+void outb(u16 port, u8 value);
+u8 inb(u16 port);
+
+#endif
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,6 +3,20 @@ void __kernel_entry()
     _print_kernel();
 }
 
+#include <onix/io.h>
+
+const static u16 VGA_WIDTH = 80;
+
+void update_cursor(int x, int y)
+{
+    u16 pos = y * VGA_WIDTH + x;
+
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (u8)(pos & 0xFF));
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (u8)((pos >> 8) & 0xFF));
+}
+
 void _print_kernel()
 {
     char *string = "Hello, Onix!!!\0";
@@ -13,4 +27,5 @@ void _print_kernel()
         *video++ = *string++;
         *video++ = color;
     }
+    update_cursor(14, 0);
 }
