4dafaf5d1c378bedb1a635ad33b87f5668a0d359
Steven Kang <kangweibaby@163.com>
2020-08-30 14:38:13 +0800

    add test process

--- src/include/onix/test.h
+++ src/include/onix/test.h
@@ -1,4 +1,8 @@
 #ifndef ONIX_TEST_H
 #define ONIX_TEST_H
 
+void test_process_a();
+void test_process_b();
+void test_process_c();
+
 #endif
--- src/kernel/test.c
+++ src/kernel/test.c
@@ -1,2 +1,22 @@
 #include <onix/test.h>
+#include <onix/type.h>
+#include <onix/stdio.h>
+#include <onix/time.h>
 
+void test_process_a()
+{
+    int i = 0;
+    while (true)
+    {
+        kprintf("This is process A %d\n\0", i++);
+        delay(10000000);
+    }
+}
+
+void test_process_b()
+{
+}
+
+void test_process_c()
+{
+}
