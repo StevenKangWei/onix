d190d5e0d818eb19fedf2751900c36ca7bc3be20
Steven Kang <kangweibaby@163.com>
2020-08-31 19:25:53 +0800

    update tty

--- src/include/onix/tty.h
+++ src/include/onix/tty.h
@@ -1,7 +1,12 @@
 #ifndef ONIX_TTY_H
 #define ONIX_TTY_H
 
+#define TTY_COMMAND_SIZE 256
+
+extern char command[TTY_COMMAND_SIZE];
+
 void task_tty();
+void init_tty();
 
 void key_process(char key);
 
--- src/kernel/tty.c
+++ src/kernel/tty.c
@@ -3,18 +3,46 @@
 #include <onix/keyboard.h>
 #include <onix/keymap.h>
 #include <onix/console.h>
+#include <onix/string.h>
+
+char command[TTY_COMMAND_SIZE];
 
 void task_tty()
 {
+    init_tty();
     while (true)
     {
         read_keyboard(key_process);
     }
 }
 
+void init_tty()
+{
+    memset(command, 0, TTY_COMMAND_SIZE);
+}
+
 void key_process(char key)
 {
-    if (key & FLAG_EXT)
+    if (!(key & FLAG_EXT))
+    {
+        put_char(&kconsole, key);
         return;
-    put_char(&kconsole, key);
+    }
+
+    int code = key & MASK_RAW;
+    switch (code)
+    {
+    case UP:
+        break;
+    case DOWN:
+        break;
+    case ENTER:
+        put_char(&kconsole, '\n');
+        break;
+    case BACKSPACE:
+        put_char(&kconsole, '\b');
+        break;
+    default:
+        break;
+    }
 }
